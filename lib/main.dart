a06c1e765520ae4464af5a7812020d5b85787b2c
cuongvq <vucuongtnnd@gmail.com>
2025-11-10 23:22:11 +0700

    push code

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:learn_app/presentation/choose_mode/bloc/theme_cubit.dart';
 import 'package:path_provider/path_provider.dart';
 
 Future<void> main() async {
@@ -11,4 +13,21 @@ Future<void> main() async {
         : HydratedStorageDirectory((await getTemporaryDirectory()).path),
   );
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (_) => ThemeCubit()),
+        ],
+        child: BlocBuilder<ThemeCubit, ThemeMode>(
+            builder: (context, mode) => MaterialApp(
+              title:'Flutter Demo',
+              theme: AppTheme,
+            )));
+  }
 }
