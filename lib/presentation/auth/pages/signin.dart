d40c3147ba408f0bb568d03d492f43db2c285715
cuongvq <vucuongtnnd@gmail.com>
2025-11-17 23:27:51 +0700

    add signin

--- lib/presentation/auth/pages/signin.dart
+++ lib/presentation/auth/pages/signin.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:learn_app/common/widgets/appbar/app_bar.dart';
 import 'package:learn_app/core/configs/app_vectors.dart';
+import 'package:learn_app/core/theme/app_color.dart';
 import 'package:learn_app/resources/app_common.dart';
 
 class SigninPage extends StatelessWidget {
   SigninPage({super.key});
 
+  final TextEditingController _fullName = TextEditingController();
   final TextEditingController _email = TextEditingController();
   final TextEditingController _password = TextEditingController();
 
@@ -21,7 +23,114 @@ class SigninPage extends StatelessWidget {
             width: AppDimens.dimen_40,
           ),
         ),
-        bottomNavigationBar: _registerText,
+        bottomNavigationBar: _registerText(),
+      ),
+    );
+  }
+
+  Widget _supportText() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const Text(
+          "If You Need Any Support ",
+          style: TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: 16,
+          ),
+        ),
+        TextButton(
+          onPressed: () {},
+          child: const Text(
+            "Click here",
+            style: TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: AppDimens.dimen_16,
+              color: AppColors.primary,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _usernameOrEmailField(BuildContext context) {
+    return TextField(
+      controller: _email,
+      decoration: const InputDecoration(
+        hintText: "Enter Username Or Email",
+      ).applyDefaults(
+        Theme.of(context).inputDecorationTheme,
+      ),
+    );
+  }
+
+  Widget _passwordField(BuildContext context) {
+    return TextField(
+      controller: _password,
+      decoration: const InputDecoration(
+        hintText: "Password",
+      ).applyDefaults(
+        Theme.of(context).inputDecorationTheme,
+      ),
+    );
+  }
+
+  Widget _recoveryPasswordText() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        TextButton(
+          onPressed: () {},
+          child: const Text(
+            "Recovery Password",
+            style: TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: AppDimens.dimen_16,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _registerText(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: AppDimens.dimen_40,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Text(
+            "Not A Member ? ",
+            style: TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: AppDimens.dimen_16,
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (BuildContext context) => SignupPage(),
+                ),
+              );
+            },
+            child: const Text(
+              "Register Now",
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: AppDimens.dimen_16,
+                color: AppColors.blue,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
