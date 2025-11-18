112a623f8d34ef7296dc62321e17acc977f56c87
cuongvq <vucuongtnnd@gmail.com>
2025-11-18 22:57:44 +0700

    add firebase continue

--- lib/data/sources/auth/auth_firebase_service.dart
+++ lib/data/sources/auth/auth_firebase_service.dart
@@ -1,8 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dartz/dartz.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:learn_app/core/constants/app_urls.dart';
 import 'package:learn_app/data/models/auth/create_user_req.dart';
 import 'package:learn_app/data/models/auth/signin_user_req.dart';
+import 'package:learn_app/data/models/auth/user.dart';
+import 'package:learn_app/domain/entities/auth/user.dart';
 
 abstract class AuthFirebaseService {
   Future<Either> signup(CreateUserReq createUserReq);
@@ -12,9 +16,30 @@ abstract class AuthFirebaseService {
 
 class AuthFirebaseServiceImpl extends AuthFirebaseService {
   @override
-  Future<Either> getUser() {
-    // TODO: implement getUser
-    throw UnimplementedError();
+  Future<Either> getUser() async {
+    try {
+      FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+      FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+
+      var user = await firebaseFirestore
+          .collection('Users')
+          .doc(firebaseAuth.currentUser?.uid)
+          .get();
+
+      UserModel userModel = UserModel.fromJson(user.data()!);
+
+      userModel.imageURL =
+          firebaseAuth.currentUser?.photoURL ?? AppUrls.defaultAvatar;
+
+      UserEntity userEntity = userModel.toEntity();
+
+      return Right(userEntity);
+    } catch (e) {
+      if (kDebugMode) {
+        print("error: $e");
+      }
+      return const Left('An error occurreed');
+    }
   }
 
   @override
